e5ff72b6203050dc427acb02af6b58e85849b6ec
Pascal Fleury <pascal@telefleuries.com>
2018-11-01 23:21:58 +0100

    remove empty file

--- org-mode-hacks.el
+++
@@ -1,5 +0,0 @@
-;; This file should contain mostly code gleaned from around the internets.
-;; It should not contain proper configuration, like keyboard bindings etc.
-;; ======================================================================
-
-;; ============================= script found to adjust the tags right-bound
