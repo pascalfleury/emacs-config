491fc32a4ef09d470287facd6b20fe422b18ca45
Pascal Fleury <pascal@telefleuries.com>
2018-11-08 00:23:41 +0100

    Cleanup of no longer needed files.

--- selective_display.el
+++
@@ -1,8 +0,0 @@
-(defun set-selective-display-dlw (&optional level)
-  "Fold text indented more than the cursor.
-   If level is set, set the indent level to level.
-   0 displays the entire buffer."
-  (interactive "P")
-  (set-selective-display (or level (current-column))))
-
-(global-set-key "\C-x$" 'set-selective-display-dlw)
