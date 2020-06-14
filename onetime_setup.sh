0b36499ac0971bbd18301db0f3d3015dee3899ac
Pascal Fleury <pascal@telefleuries.com>
2020-06-14 21:42:49 +0200

    Added package installer and sqlite3 for org-roam.

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# This is a bit of heuristics to find out what the install system is
+PKG_MGRS=( "apt-get" "pkg" )
+for pkg in "${PKG_MGRS[@]}"l do
+    if [[ "-x $(which ${pkg})" ]]; then
+        INSTALLER="$(which ${pkg})"
+        break
+    fi
+done
+if [[ -z "${INSTALLER}" ]]; then
+    echo "Did not find a suitable installer..."
+    exit 1
+fi
+
 # Make git ignore the tangled & updated emacs_setup.el
 GIT_ROOT=$(dirname $0)
 (cd ${GIT_ROOT} && git update-index --skip-worktree emacs_setup.el)
@@ -20,8 +33,10 @@ else
   echo "Config in your ~/.emacs already set up!"
 fi
 
+# org-roam needs this binary
+${INSTALLER} install sqlite3
 # Make sure there is a C compiler for emacsql-sqlite
-[[ -n "$(which cc)" ]] || pkg install clang
+[[ -n "$(which cc)" ]] || ${INSTALLER} install clang
 
 # Install reveal.js
 (cd ~/ && git clone https://github.com/hakimel/reveal.js.git)
