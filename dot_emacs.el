ba6878f331c42cb844f793482d7e36eb707225c5
Pascal Fleury <fleury@google.com>
2020-07-16 00:36:32 +0200

    Reorganize my setup file. Enables clanup of .emacs too

--- dot_emacs.el
+++ dot_emacs.el
@@ -1,13 +1,2 @@
 ;; Loads PAF's emacs setup with bootstrap
 (load-file "~/Emacs/emacs_setup.el")
-
-;; faces for general region highlighting zenburn is too low-key.
-(custom-set-faces
- '(highlight ((t (:background "forest green"))))
- '(region ((t (:background "forest green")))))
-
-;; Faces to make the calendar more colorful.
-(custom-set-faces
- '(org-agenda-current-time ((t (:inherit org-time-grid :foreground "yellow" :weight bold))))
- '(org-agenda-date ((t (:inherit org-agenda-structure :background "pale green" :foreground "black" :weight bold))))
- '(org-agenda-date-weekend ((t (:inherit org-agenda-date :background "light blue" :weight bold)))))
