a516a0d2753144fa5352d33d3907b1d4bf99d019
Pascal Fleury <pascal@telefleuries.com>
2020-11-12 22:41:52 +0100

    More clanup and less files materialized.

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -4,11 +4,20 @@ if [[ -z "$(which git)" ]]; then
   echo "You will need 'git' to be installed !"
   exit 1
 fi
+if  [[ -z "$(which emacs)" ]]; then
+  echo "You might need 'emacs' for this to be useful !"
+  exit 1
+fi
+
 GIT_ROOT=$(dirname $0)
 (cd ${GIT_ROOT} && git update-index --skip-worktree emacs_setup.el)
 
 # Maybe this is a new install, .emacs does not exist
 test -e ~/.emacs || touch ~/.emacs
+
+# Initial tangle of files.
+emacs --batch --load ${GIT_ROOT}/emacs_setup.el
+GENERATED=/tmp  # where it creates files
 
 # Add the load-file as the first thing in the user's ~/.emacs
 # If not yet added.
@@ -16,9 +25,12 @@ declare lines=$(grep ';; dot_emacs.el' ~/.emacs | wc -l)
 if (( lines < 1 )); then
   echo "Added loading the config in your ~/.emacs"
   echo ";; dot_emacs.el" > ~/.emacs.new
-  cat ${GIT_ROOT}/dot_emacs.el >> ~/.emacs.new
+  cat ${GENERATED}/dot_emacs.el >> ~/.emacs.new
   cat ~/.emacs >> ~/.emacs.new
   mv ~/.emacs.new ~/.emacs
 else
   echo "Config in your ~/.emacs already set up!"
 fi
+
+# Install system dependencies
+bash ${GENERATED}/onetime_setup.sh
