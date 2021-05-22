5a9553cbc03fc9b0c507c7e29b8ecbaff1987cbe
Pascal Fleury <pascal@telefleuries.com>
2021-05-22 22:17:10 +0200

    Added support for go/ links.

--- hyperbole-systems.el
+++ hyperbole-systems.el
@@ -17,6 +17,17 @@
 (put 'systems-link 'bounds-of-thing-at-point
      'systems-link-bounds-of-thing-at-point)
 
+(defun short-link-bounds-of-thing-at-point ()
+  "Find constructs that are identifiers for short links, go/... or goto/..."
+  (save-excursion
+    (skip-chars-backward "0123456789/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_/?#$=,;.&")
+    (if (looking-at "got?o?/[^ \t\n]+")
+        (cons (point) (match-end 0))
+      nil)))
+
+(put 'short-link 'bounds-of-thing-at-point
+     'short-link-bounds-of-thing-at-point)
+
 (defun systems-ldap-bounds-of-thing-at-point ()
   "Find constructs that are people usernames like john@ or jack@"
   (save-excursion
@@ -45,11 +56,14 @@
   (save-excursion
     (let ((mid (thing-at-point 'mid))
           (link (thing-at-point 'systems-link))
+          (short (thing-at-point 'short-link))
           (ldap (thing-at-point 'systems-ldap))
           topic)
       (cond (mid (progn (ibut:label-set mid)
                         (hact 'www-url (concat "http://lexistore" mid))))
             (link (progn (ibut:label-set link)
                          (hact 'www-url (concat "http://" link))))
+            (link (progn (ibut:label-set short)
+                         (hact 'www-url (concat "http://" short))))
             (ldap (progn (ibut:label-set ldap)
                          (hact 'www-url (concat "http://who/" ldap))))))))
