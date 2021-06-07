5f7a6ec0f32e2a40e283ee6209d1e1860c902a6a
Pascal Fleury <fleury@users.sourceforge.net>
2021-06-07 22:58:45 +0200

    Fix the movein script to actually work.

--- Bootstrap/movein.sh
+++ Bootstrap/movein.sh
@@ -4,8 +4,8 @@ readonly HS_REPOS="${HOME}/.homesick/repos"
 
 [ -d "${HS_REPOS}" ] || mkdir -p "${HS_REPOS}" \
 && cd "${HS_REPOS}" \
-&& git clone https://github.com/Billiam/homeshick.git \
-alias homeshick="${HS_REPOS}/homeshick/home/.homeshick"
+&& git clone https://github.com/Billiam/homeshick.git
 
-homeshick clone https://github.com/pascalfleury/dotfiles.git
-homeshick clone https://github.com/pascalfleury/shelltoys.git
+export homeshick="${HS_REPOS}/homeshick/home/.homeshick"
+$homeshick clone https://github.com/pascalfleury/dotfiles.git
+$homeshick clone https://github.com/pascalfleury/shelltoys.git
