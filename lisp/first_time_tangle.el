42d7d8a667ea53891bfb8e6f4974e56b76b83c28
Pascal Fleury <pascal@telefleuries.com>
2021-09-13 22:29:17 +0200

    Tangle directly into the ~/.emacs.d directory.

---
+++ lisp/first_time_tangle.el
@@ -0,0 +1,11 @@
+;; This is the initial state of the file to be loaded.
+;; It will replace itself with the actual configuration at first run.
+
+(require 'org) ; We can't tangle without org!
+
+(setq config_base (expand-file-name "emacs_setup"
+				    (file-name-directory
+				     (or load-file-name buffer-file-name))))
+(find-file (concat config_base ".org"))  ; Open the configuration
+(org-babel-tangle)                       ; tangle it
+(load-file "~/.emacs.d/init.el")         ; load it
--- onetime_setup.sh
+++ onetime_setup.sh
@@ -1,3 +1,4 @@
+# [[file:emacs_setup.org::*Script to bootstrap the whole thing on a new system][Script to bootstrap the whole thing on a new system:1]]
 #!/bin/bash
 # Make git ignore the tangled & updated emacs_setup.el
 if [[ -z "$(which git)" ]]; then
@@ -10,31 +11,31 @@ if  [[ -z "$(which emacs)" ]]; then
 fi
 
 GIT_ROOT=$(dirname $0)
-(cd ${GIT_ROOT} && git update-index --skip-worktree emacs_setup.el)
 
 # Maybe this is a new install, .emacs does not exist
 test -e ~/.emacs || touch ~/.emacs
 
 # Initial tangle of files, saying no to vterm compilation
-emacs --batch --load ${GIT_ROOT}/emacs_setup.el
+emacs --batch --load "${GIT_ROOT}/lisp/first_time_tangle.el"
 
 # Add the load-file as the first thing in the user's ~/.emacs
 # If not yet added.
 declare lines=$(grep ';; dot_emacs.el' ~/.emacs | wc -l)
 if (( lines < 1 )); then
-  echo "Added loading the config in your ~/.emacs"
   echo ";; dot_emacs.el" > ~/.emacs.new
-  cat ${GIT_ROOT}/dot_emacs.el >> ~/.emacs.new
+  cat "${GIT_ROOT}/dot_emacs.el" >> ~/.emacs.new
   cat ~/.emacs >> ~/.emacs.new
   mv ~/.emacs.new ~/.emacs
+  echo "Added loading the config in your ~/.emacs"
 else
   echo "Config in your ~/.emacs already set up!"
 fi
 
-# Install system dependencies
+# Install system dependencies from the tangled script
 echo "Installing dependencies"
 bash ${GIT_ROOT}/install_deps.sh
 
 echo "Cleanup"
 rm ${GIT_ROOT}/dot_emacs.el
 rm ${GIT_ROOT}/install_deps.sh
+# Script to bootstrap the whole thing on a new system:1 ends here
