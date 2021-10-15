8faebad7dd09aa09d3b83a43ae74cb805e83e5c0
Pascal Fleury <pascal@telefleuries.com>
2021-10-15 23:24:41 +0200

    find the right file.

--- lisp/first_time_tangle.el
+++ lisp/first_time_tangle.el
@@ -3,10 +3,9 @@
 
 (require 'org) ; We can't tangle without org!
 
-(setq config_base (expand-file-name "emacs_setup"
-				    (file-name-directory ;; because we're in 'lisp' dir
-                                     (file-name-directory
-				      (or load-file-name buffer-file-name)))))
+(setq config_base (expand-file-name "../emacs_setup" ;; because we're in the 'lisp' dir.
+                                    (file-name-directory
+				      (or load-file-name buffer-file-name))))
 (message "Tangling '%s' ..." (concat config_base ".org"))
 (find-file (concat config_base ".org"))  ; Open the configuration
 (org-babel-tangle)                       ; tangle it
