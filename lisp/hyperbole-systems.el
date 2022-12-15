9c48a623eed04b5c3786b112baa743a0ebf0cf3d
P. Fleury <fleury@users.sourceforge.net>
2022-12-15 17:02:38 +0100

    Added CNS and SrcFS links. Also simplified some expressions.

--- lisp/hyperbole-systems.el
+++ lisp/hyperbole-systems.el
@@ -9,7 +9,7 @@
 (defun systems-link-bounds-of-thing-at-point ()
   "Find constructs that are identifiers for internal systems, e.g. b/12345 cr/12345678 guts/12345"
   (save-excursion
-    (skip-chars-backward "0123456789/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
+    (skip-chars-backward "0-9a-zA-Z_/")
     (if (looking-at "[a-zA-Z_]+/[0-9a-zA-Z_-]+")
         (cons (point) (match-end 0))
       nil))) ; not a systems thing
@@ -20,7 +20,7 @@
 (defun short-link-bounds-of-thing-at-point ()
   "Find constructs that are identifiers for short links, go/... or goto/..."
   (save-excursion
-    (skip-chars-backward "0123456789/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_/?#$=,;.&-")
+    (skip-chars-backward "0-9a-zA-Z_/?#$=,;.&-")
     (if (looking-at "got?o?/[[:alnum:]_-]+\([?/#][^ \t\n]*\)?")
         (cons (point) (match-end 0))
       nil)))
@@ -31,8 +31,8 @@
 (defun systems-ldap-bounds-of-thing-at-point ()
   "Find constructs that are people usernames like john@ or jack@"
   (save-excursion
-    (skip-chars-backward "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@")
-    (if (looking-at "[a-zA-Z_]+@")
+    (skip-chars-backward "a-zA-Z\.@")
+    (if (looking-at "[a-zA-Z_\.]+@")
         (cons (point) (1- (match-end 0)))
       nil))) ; not a systems username
 
@@ -42,7 +42,7 @@
 (defun mid-bounds-of-thing-at-point ()
   "Find constructs that are mids, like /g/126534 or /m/01yrx."
   (save-excursion
-    (skip-chars-backward "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_/")
+    (skip-chars-backward "0-9a-zA-Z_/")
     (if (looking-at "/[mgtxpn]/[a-z0-9_]+")
         (cons (point) (match-end 0))
       nil))) ; not a mid
@@ -50,6 +50,27 @@
 (put 'mid 'bounds-of-thing-at-point
      'mid-bounds-of-thing-at-point)
 
+(defun srcfs-path-of-thing-at-point ()
+  "Find constructs that are paths to SrcFS."
+  (save-excursion
+    (skip-chars-backward "0-9a-zA-Z_/.-")
+    (if (looking-at "//[[:alnum:]_/.-]+")
+        (cons (+ (point) 2) (match-end 0)) ; remove one leading slashes
+      nil))) ; not a SrcFS path
+
+(put 'srcfs 'bounds-of-thing-at-point
+     'srcfs-path-of-thing-at-point)
+
+(defun cns-path-of-thing-at-point ()
+  "Find constructs that are paths to CNS."
+  (save-excursion
+    (skip-chars-backward "0-9a-zA-Z_/%=.-")
+    (if (looking-at "/cns/[[:alnum:]_/%=\.-]+")
+        (cons (point) (match-end 0))
+      nil))) ; not a CNS path
+
+(put 'cns 'bounds-of-thing-at-point
+     'cns-path-of-thing-at-point)
 
 (defib systems-stuff ()
   "Hyperbole implicit button for systems style things."
@@ -59,14 +80,22 @@
           (short (thing-at-point 'short-link))
           (ldap (thing-at-point 'systems-ldap))
           (mid (thing-at-point 'mid))
+          (srcfs (thing-at-point 'srcfs))
+          (cns (thing-at-point 'cns))
           topic)
-      (cond (mid (progn (ibut:label-set mid)
-                        (hact 'www-url (concat "http://lexistore" mid))))
-            (short (progn (ibut:label-set short)
-                          (hact 'www-url (concat "http://" short))))
-            (link (progn (ibut:label-set link)
-                         (hact 'www-url (concat "http://" link))))
-            (ldap (progn (ibut:label-set ldap)
-                         (hact 'www-url (concat "http://who/" ldap))))
-            (mid (progn (ibut:label-set mid)
-                         (hact 'www-url (concat "http://hume" mid))))))))
+      (cond
+       (cns (progn (ibut:label-set cns)
+                   (hact 'www-url (concat "http://data.corp.google.com/cnsviewer/file?query=" cns))))
+       (mid (progn (ibut:label-set mid)
+                   (hact 'www-url (concat "http://lexistore" mid))))
+       (short (progn (ibut:label-set short)
+                     (hact 'www-url (concat "http://" short))))
+       (link (progn (ibut:label-set link)
+                    (hact 'www-url (concat "http://" link))))
+       (ldap (progn (ibut:label-set ldap)
+                    (hact 'www-url (concat "http://who/" ldap))))
+       (mid (progn (ibut:label-set mid)
+                   (hact 'www-url (concat "http://hume" mid))))
+       (srcfs (progn (ibut:label-set srcfs)
+                     (hact 'www-url (concat "http://cs///depot/google3/" srcfs))))
+       ))))
